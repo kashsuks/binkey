2dc004412d5e6e4394ff16f05741032338ed62fc
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-07-04 18:23:39 -0400

    added everything

---
+++ code/simpleCounter.ino
@@ -0,0 +1,15 @@
+const int BUT1 = D1;
+const int BUT2 = D2;
+const int BUT3 = D3;
+const int BUT4 = D4;
+
+const int LED1 = 5;
+const int LED2 = 6;
+const int LED3 = 7;
+const int LED4 = 8;
+
+
+// Array with all the buttons and led's so we can flash in sync and count using binary!!!!
+const int BUTTONS[4] = {BUT1, BUT2, BUT3, BUT4};
+const int LEDS[4] = {LED1, LED2, LED3, LED4};
+
